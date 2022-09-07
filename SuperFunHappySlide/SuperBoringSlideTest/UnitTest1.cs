ded0c176b2b3bddc2f9308c62f49c1522ee40c62
Leif Boelskift (LEBF) <lebf@demant.com>
2022-09-07 15:35:23 +0200

    with test added

---
+++ SuperFunHappySlide/SuperBoringSlideTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace SuperBoringSlideTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsNotNull("bla");
+        }
+    }
+}
