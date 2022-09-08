b4f96c3c6cbbc0f50794928c39039cc28f409c65
Leif Boelskift (LEBF) <lebf@demant.com>
2022-09-08 15:35:10 +0200

    template test

---
+++ Scripts/CreateDummyFile.ps1
@@ -0,0 +1,15 @@
+﻿[cmdletbinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $BasePath,
+    [Parameter(Mandatory = $true)]
+    [string] $DummyFileName
+
+)
+
+$fullPath = "$BasePath\$DummyFileName"
+
+
+$f = new-object System.IO.FileStream $fullPath, Create, ReadWrite
+$f.SetLength(1MB)
+$f.Close()
