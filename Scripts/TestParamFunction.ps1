698338ebfbf9938b0f0dbeae4e99fa9765707e46
Leif Boelskift (LEBF) <lebf@demant.com>
2022-09-08 12:07:51 +0200

    ps testing

---
+++ Scripts/TestParamFunction.ps1
@@ -0,0 +1,20 @@
+﻿[cmdletbinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $InputParam
+
+)
+
+
+
+function GenerateDocument{
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $Param
+
+    )
+    Write-Host "the input parameter is : $Param"
+}
+
+
+ GenerateDocument -Param $InputParam
